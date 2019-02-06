f185a4dec6cb924cabed2be157636d91e3ef234e
Daniel P. Purkhus <purkhusid@gmail.com>
2019-02-06 22:05:46 +0000

    Ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+set -o pipefail
+set -o errexit
+
+sudo apt update -y
+sudo apt install -y \
+git \
+vim \
+apt-transport-https \
+ca-certificates \
+curl \
+wget \
+software-properties-common 
+
+#Drivers
+sudo ubuntu-drivers autoinstall
+
+#Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update -y
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+
+#VS Code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update -y
+sudo apt install -y code
+
+#Steam
+sudo add-apt-repository multiverse
+sudo apt update -y
+sudo apt install -y steam
+
+#Intellij
+sudo snap install intellij-idea-community --classic --edge
+sudo snap install rider --classic --edge
+sudo snap install spotify
